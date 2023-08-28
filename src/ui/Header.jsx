b0a64657a535bcebd85308b20bb8c52987ebd672
armangral <91218206+armangral@users.noreply.github.com>
2023-08-29 00:44:41 +0500

    building app layout

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <Link to="/">Fast React Pizza</Link>
+      <p>hahha</p>
+    </header>
+  );
 }
 
 export default Header;
