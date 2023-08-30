15b0be1e8ed23392b3d31d7a68dc9cae9711538d
armangral <91218206+armangral@users.noreply.github.com>
2023-08-30 13:53:26 +0500

    adding color and text styling

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">MANGRAL</div>;
+}
+
+export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,14 +8,15 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen  grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <h1>main content</h1>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className=" mx-auto max-w-3xl bg-red-500">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
