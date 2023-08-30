bb94a4dc2af65716d144cc5ab12bf42ac287aaf1
armangral <91218206+armangral@users.noreply.github.com>
2023-08-30 23:14:31 +0500

    reusing styles

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -49,29 +50,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input
-            type="text"
-            name="customer"
-            required
-            className="w-full rounded-full border border-stone-200 px-4
-                        py-2 text-sm transition-all duration-300
-                        placeholder:text-stone-400 focus:outline-none focus:ring
-                         focus:ring-yellow-400 md:px-6 md:py-3 "
-          />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input
-              type="tel"
-              name="phone"
-              required
-              className="w-full rounded-full border border-stone-200 px-4
-                        py-2 text-sm transition-all duration-300
-                        placeholder:text-stone-400 focus:outline-none focus:ring
-                         focus:ring-yellow-400 md:px-6 md:py-3 "
-            />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -79,15 +64,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="w-full rounded-full border border-stone-200 px-4
-                        py-2 text-sm transition-all duration-300
-                        placeholder:text-stone-400 focus:outline-none focus:ring
-                         focus:ring-yellow-400 md:px-6 md:py-3 "
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
@@ -108,16 +85,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-full  bg-yellow-400 px-4 py-3 font-semibold 
-            uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300
-            focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300
-            focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
-            {" "}
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing Order... " : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
