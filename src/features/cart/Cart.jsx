261d86954f979769792061096059901648e5a75d
armangral <91218206+armangral@users.noreply.github.com>
2023-09-07 19:35:11 +0500

    updating cart quantities

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -51,7 +51,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200">
         {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
